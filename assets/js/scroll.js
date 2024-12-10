cb5d4e1effaaefcc055b242eaa685bbf777d526c
Joss-Acosta <joss.acosta2@outlook.fr>
2024-12-10 22:05:29 +0100

    Delete assets directory

--- assets/js/scroll.js
+++
@@ -1,7 +0,0 @@
-function scrollToElement(id) {
-    var element = document.getElementById(id);
-    window.scrollTo({
-      top: element.offsetTop - 100, // Ajuste la valeur pour déplacer la section plus haut ou plus bas
-      behavior: 'smooth'
-    });
-  }
